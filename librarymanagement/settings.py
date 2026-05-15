2a70a573bb39ae43abf7097975c2f42e0fb58824
Noor Butt <nooramir.nb@gmail.com>
2026-05-15 16:01:09 +0500

    Update settings.py

--- librarymanagement/settings.py
+++ librarymanagement/settings.py
@@ -138,7 +138,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
 
 # Allauth
