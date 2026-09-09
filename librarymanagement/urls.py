8e7179ec7a6ac8b488cc22d507054d76c8ada715
Noor Butt <nooramir.nb@gmail.com>
2026-09-10 00:08:19 +0500

    fixed domain verification for strix

--- librarymanagement/urls.py
+++ librarymanagement/urls.py
@@ -3,8 +3,18 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+
+def strix_verify(request):
+    return HttpResponse(
+        "strix-verify-57268871b985d3c921745e1204c70f53",
+        content_type="text/plain",
+    )
+
 
 urlpatterns = [
+    path(".well-known/strix-verify.txt", strix_verify),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("", include("apps.core.urls")),
